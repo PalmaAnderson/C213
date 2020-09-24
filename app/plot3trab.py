44ff667df0ebf031dd547ee5de497e33ef5711ee
PalmaAnderson <palma.anderson1@gmail.com>
2020-09-24 18:22:06 -0300

    Add files via upload

--- app/plot3trab.py
+++ app/plot3trab.py
@@ -98,7 +98,7 @@ def plot(modo,a1,b1,sp,p,i,d):
     ax.set(xlabel='Amostras', ylabel='Valores', title=titulo)
     ax.grid()
     #plt.show()
-    fig.savefig("./tmp/Results.png")
+    fig.savefig("/tmp/Results.png")
 
     
     Html=create_table(modo,a1,b1,sp,p,i,d,pico)
